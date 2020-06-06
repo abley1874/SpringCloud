d65596acbd8ed1ca5003507fd1e72704af4f31b4
dingyi <abley1874@163.com>
2020-06-06 21:55:03 +0800

    24 服务发现 Discovery

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,25 +4,31 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class PaymentController {
 
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
     @Value("${server.port}")
     private String serverPort;
-
     @Resource
     private PaymentService paymentService;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
 
         int result = paymentService.crate(payment);
-
         if (result > 0) {
             return new CommonResult(200, "插入数据成功,serverPort:" + serverPort, result);
         } else {
@@ -40,6 +46,21 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应数据", null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            logger.info("******service:" + service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            logger.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 
 
