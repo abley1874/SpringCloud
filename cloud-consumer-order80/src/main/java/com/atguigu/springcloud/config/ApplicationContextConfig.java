6064a491b4dd27831632d14a123e3549cd46eacc
dingyi <abley1874@163.com>
2020-06-06 17:37:02 +0800

    日常更新

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean   //相当于 Spring中 applicationContext.xml<bean id="" class="">
+    @LoadBalanced   //赋予RestTemplate具有负载均衡的能力
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentMapper;
+
+    @Override
+    public int crate(Payment payment) {
+        return paymentMapper.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentMapper.getPaymentById(id);
+    }
+}
