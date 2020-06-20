1f1304b7cfa638f9a6a43906d5b7e2d331d3b1f6
dingyi <abley1874@163.com>
2020-06-20 18:48:30 +0800

    服务总线 Bus 实现自动动态刷新服务配置文件 https://www.bilibili.com/video/BV18E411x7eT?p=78

---
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
